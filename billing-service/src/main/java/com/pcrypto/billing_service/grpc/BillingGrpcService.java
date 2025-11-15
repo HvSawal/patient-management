515c63f71e2416324ebdd40c3f7e3e3b90523ceb
Harshvardhan Sawal <hv.sawal@gmail.com>
2025-11-14 22:59:21 -0500

    billing-service and grpc protobuf added

---
+++ billing-service/src/main/java/com/pcrypto/billing_service/grpc/BillingGrpcService.java
@@ -0,0 +1,28 @@
+package com.pcrypto.billing_service.grpc;
+
+import billing.BillingResponse;
+import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.server.service.GrpcService;
+import billing.BillingServiceGrpc.BillingServiceImplBase;
+
+@Slf4j
+@GrpcService
+public class BillingGrpcService extends BillingServiceImplBase {
+
+    @Override
+    public void createBillingAccount(billing.BillingRequest billingRequest, StreamObserver<billing.BillingResponse> responseObserver){
+
+        log.info("createBillingAccount request received : {}", billingRequest.toString());
+
+        // Business logic - eg save to database, perform calculations
+
+        BillingResponse response = BillingResponse.newBuilder()
+                .setAccountId("12345")
+                .setStatus("ACTIVE")
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
