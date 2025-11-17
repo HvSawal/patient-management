e49ebcc80889a4b183cdf953c2e29129c06d611b
Harshvardhan Sawal <hv.sawal@gmail.com>
2025-11-17 15:14:09 -0500

    PR feedback, cleanup

--- api-gateway/src/main/java/com/pcrypto/api_gateway/filter/JwtValidationGatewayFilterFactory.java
+++ api-gateway/src/main/java/com/pcrypto/api_gateway/filter/JwtValidationGatewayFilterFactory.java
@@ -22,7 +22,7 @@ public class JwtValidationGatewayFilterFactory extends AbstractGatewayFilterFact
         return (exchange, chain) -> {
             String token = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
 
-            if(token == null || !token.startsWith("Bearer ")){
+            if (token == null || !token.startsWith("Bearer ")) {
                 exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
                 return exchange.getResponse().setComplete();
             }
